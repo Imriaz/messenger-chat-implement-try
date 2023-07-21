b98534dbda8187ecfd4a0d4b8c5dd8fb00c5174b
Imriaz Uddin <imriaz.uddin@bjitgroup.com>
2023-07-21 12:22:28 +0600

    update-6

--- components/messenger-live-chat/index.js
+++ components/messenger-live-chat/index.js
@@ -3,7 +3,7 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 
 const MessengerLiveChat = () => {
   return (
-    <FacebookProvider appId="238792998887533" chatSupport>
+    <FacebookProvider appId="2302895319880854" chatSupport>
       <CustomChat pageId="100443455067967" />
     </FacebookProvider>
   );
