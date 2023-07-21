f497f8d5093e8e49af5c704a38d4f2a2fe92a56c
Imriaz Uddin <imriaz.uddin@bjitgroup.com>
2023-07-21 09:09:18 +0600

    Implement messenger chat

---
+++ app/contact-us/page.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ContactUs = () => {
+  return (
+    <div>
+      <p
+        style={{
+          border: "2px solid black",
+          padding: "10px",
+          backgroundColor: "skyblue",
+        }}
+      >
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi nisi rem
+        facilis culpa saepe doloremque qui asperiores accusantium aperiam
+        similique vel mollitia alias aliquid, quam iure ratione, molestiae
+        optio. Totam ex harum neque dolore doloribus dignissimos voluptates
+        distinctio ullam, ea dolorum corrupti ratione cupiditate aperiam eius
+        molestias ipsam mollitia a.
+      </p>
+    </div>
+  );
+};
+
+export default ContactUs;
